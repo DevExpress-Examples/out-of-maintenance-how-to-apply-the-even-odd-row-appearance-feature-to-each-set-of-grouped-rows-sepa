979c9f8adfb01eb22ec1e9b7df3838e086ca93a7
Donate_ro <joke4789@ya.ru>
2019-05-20 17:09:46 +0300

    DataSource is replaced from the database file to the dynamical DataTable object

--- CS/S131432/Form1.cs
+++ CS/S131432/Form1.cs
@@ -1,33 +1,50 @@
+using DevExpress.XtraGrid.Views.Grid;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
-using DevExpress.XtraGrid.Views.Grid;
 
-namespace S131432 {
-    public partial class Form1 : Form {
-        public Form1() {
+namespace S131432
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e) {
-            // TODO: This line of code loads data into the 'nwindDataSet.Products' table. You can move, or remove it, as needed.
-            this.productsTableAdapter.Fill(this.nwindDataSet.Products);
-
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            productsBindingSource.DataSource = GetProductsDataTable();
         }
-
-        private void OnGridViewRowStyle(object sender, RowStyleEventArgs e) {
+        DataTable GetProductsDataTable()
+        {
+            DataTable table = new DataTable();
+            table.TableName = "Products";
+            table.Columns.Add(new DataColumn("ProductID", typeof(int)));
+            table.Columns.Add(new DataColumn("ProductName", typeof(string)));
+            table.Columns.Add(new DataColumn("SupplierID", typeof(int)));
+            table.Columns.Add(new DataColumn("CategoryID", typeof(string)));
+            table.Columns.Add(new DataColumn("QuantityPerUnit", typeof(int)));
+            for(int i = 0; i < 20; i++)
+            {
+                int index = i + 1;
+                table.Rows.Add(index, "Product " + index, index * 2, "Category " + i % 8, index % 7);
+            }
+            return table;
+        }
+        private void OnGridViewRowStyle(object sender, RowStyleEventArgs e)
+        {
             GridView gView = (GridView)sender;
-            if (!gView.IsValidRowHandle(e.RowHandle)) return;
+            if(!gView.IsValidRowHandle(e.RowHandle)) return;
             int parent = gView.GetParentRowHandle(e.RowHandle);
-            if (gView.IsGroupRow(parent)) {
-                for (int i = 0; i < gView.GetChildRowCount(parent); i++)
-                    if (gView.GetChildRowHandle(parent, i) == e.RowHandle)
+            if(gView.IsGroupRow(parent))
+            {
+                for(int i = 0; i < gView.GetChildRowCount(parent); i++)
+                    if(gView.GetChildRowHandle(parent, i) == e.RowHandle)
                         e.Appearance.BackColor = i % 2 == 0 ? Color.AliceBlue : Color.AntiqueWhite;
-            } else e.Appearance.BackColor = e.RowHandle % 2 == 0 ? Color.AliceBlue : Color.AntiqueWhite;
+            }
+            else e.Appearance.BackColor = e.RowHandle % 2 == 0 ? Color.AliceBlue : Color.AntiqueWhite;
         }
     }
 }
--- VB/S131432/Form1.Designer.vb
+++ VB/S131432/Form1.Designer.vb
@@ -1,151 +1,88 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace S131432
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+    Partial Class Form1
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
 
-		#Region "Windows Form Designer generated code"
+            MyBase.Dispose(disposing)
+        End Sub
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
-			Me.productsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-			Me.nwindDataSet = New S131432.nwindDataSet()
-			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-			Me.colProductID = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colProductName = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colSupplierID = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colCategoryID = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colQuantityPerUnit = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.productsTableAdapter = New S131432.nwindDataSetTableAdapters.ProductsTableAdapter()
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.productsBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.nwindDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' gridControl1
-			' 
-			Me.gridControl1.DataSource = Me.productsBindingSource
-			Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.gridControl1.EmbeddedNavigator.Name = ""
-			Me.gridControl1.Location = New System.Drawing.Point(0, 0)
-			Me.gridControl1.MainView = Me.gridView1
-			Me.gridControl1.Name = "gridControl1"
-			Me.gridControl1.Size = New System.Drawing.Size(772, 450)
-			Me.gridControl1.TabIndex = 0
-			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
-			' 
-			' productsBindingSource
-			' 
-			Me.productsBindingSource.DataMember = "Products"
-			Me.productsBindingSource.DataSource = Me.nwindDataSet
-			' 
-			' nwindDataSet
-			' 
-			Me.nwindDataSet.DataSetName = "nwindDataSet"
-			Me.nwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-			' 
-			' gridView1
-			' 
-			Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colProductID, Me.colProductName, Me.colSupplierID, Me.colCategoryID, Me.colQuantityPerUnit})
-			Me.gridView1.GridControl = Me.gridControl1
-			Me.gridView1.GroupCount = 1
-			Me.gridView1.Name = "gridView1"
-			Me.gridView1.SortInfo.AddRange(New DevExpress.XtraGrid.Columns.GridColumnSortInfo() { New DevExpress.XtraGrid.Columns.GridColumnSortInfo(Me.colCategoryID, DevExpress.Data.ColumnSortOrder.Ascending)})
-'			Me.gridView1.RowStyle += New DevExpress.XtraGrid.Views.Grid.RowStyleEventHandler(Me.OnGridViewRowStyle);
-			' 
-			' colProductID
-			' 
-			Me.colProductID.Caption = "ProductID"
-			Me.colProductID.FieldName = "ProductID"
-			Me.colProductID.Name = "colProductID"
-			Me.colProductID.Visible = True
-			Me.colProductID.VisibleIndex = 0
-			' 
-			' colProductName
-			' 
-			Me.colProductName.Caption = "ProductName"
-			Me.colProductName.FieldName = "ProductName"
-			Me.colProductName.Name = "colProductName"
-			Me.colProductName.Visible = True
-			Me.colProductName.VisibleIndex = 1
-			' 
-			' colSupplierID
-			' 
-			Me.colSupplierID.Caption = "SupplierID"
-			Me.colSupplierID.FieldName = "SupplierID"
-			Me.colSupplierID.Name = "colSupplierID"
-			Me.colSupplierID.Visible = True
-			Me.colSupplierID.VisibleIndex = 2
-			' 
-			' colCategoryID
-			' 
-			Me.colCategoryID.Caption = "CategoryID"
-			Me.colCategoryID.FieldName = "CategoryID"
-			Me.colCategoryID.Name = "colCategoryID"
-			Me.colCategoryID.Visible = True
-			Me.colCategoryID.VisibleIndex = 3
-			' 
-			' colQuantityPerUnit
-			' 
-			Me.colQuantityPerUnit.Caption = "QuantityPerUnit"
-			Me.colQuantityPerUnit.FieldName = "QuantityPerUnit"
-			Me.colQuantityPerUnit.Name = "colQuantityPerUnit"
-			Me.colQuantityPerUnit.Visible = True
-			Me.colQuantityPerUnit.VisibleIndex = 3
-			' 
-			' productsTableAdapter
-			' 
-			Me.productsTableAdapter.ClearBeforeFill = True
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(772, 450)
-			Me.Controls.Add(Me.gridControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-'			Me.Load += New System.EventHandler(Me.Form1_Load);
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.productsBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.nwindDataSet, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+            Me.productsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+            Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.colProductID = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colProductName = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colSupplierID = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colCategoryID = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colQuantityPerUnit = New DevExpress.XtraGrid.Columns.GridColumn()
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.productsBindingSource), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            Me.gridControl1.DataSource = Me.productsBindingSource
+            Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.gridControl1.EmbeddedNavigator.Name = ""
+            Me.gridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.gridControl1.MainView = Me.gridView1
+            Me.gridControl1.Name = "gridControl1"
+            Me.gridControl1.Size = New System.Drawing.Size(772, 450)
+            Me.gridControl1.TabIndex = 0
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gridView1})
+            Me.productsBindingSource.DataMember = "Products"
+            Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.colProductID, Me.colProductName, Me.colSupplierID, Me.colCategoryID, Me.colQuantityPerUnit})
+            Me.gridView1.GridControl = Me.gridControl1
+            Me.gridView1.GroupCount = 1
+            Me.gridView1.Name = "gridView1"
+            Me.gridView1.SortInfo.AddRange(New DevExpress.XtraGrid.Columns.GridColumnSortInfo() {New DevExpress.XtraGrid.Columns.GridColumnSortInfo(Me.colCategoryID, DevExpress.Data.ColumnSortOrder.Ascending)})
+            Me.colProductID.Caption = "ProductID"
+            Me.colProductID.FieldName = "ProductID"
+            Me.colProductID.Name = "colProductID"
+            Me.colProductID.Visible = True
+            Me.colProductID.VisibleIndex = 0
+            Me.colProductName.Caption = "ProductName"
+            Me.colProductName.FieldName = "ProductName"
+            Me.colProductName.Name = "colProductName"
+            Me.colProductName.Visible = True
+            Me.colProductName.VisibleIndex = 1
+            Me.colSupplierID.Caption = "SupplierID"
+            Me.colSupplierID.FieldName = "SupplierID"
+            Me.colSupplierID.Name = "colSupplierID"
+            Me.colSupplierID.Visible = True
+            Me.colSupplierID.VisibleIndex = 2
+            Me.colCategoryID.Caption = "CategoryID"
+            Me.colCategoryID.FieldName = "CategoryID"
+            Me.colCategoryID.Name = "colCategoryID"
+            Me.colCategoryID.Visible = True
+            Me.colCategoryID.VisibleIndex = 3
+            Me.colQuantityPerUnit.Caption = "QuantityPerUnit"
+            Me.colQuantityPerUnit.FieldName = "QuantityPerUnit"
+            Me.colQuantityPerUnit.Name = "colQuantityPerUnit"
+            Me.colQuantityPerUnit.Visible = True
+            Me.colQuantityPerUnit.VisibleIndex = 3
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0F, 13.0F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(772, 450)
+            Me.Controls.Add(Me.gridControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.productsBindingSource), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		End Sub
-
-		#End Region
-
-		Private gridControl1 As DevExpress.XtraGrid.GridControl
-		Private WithEvents gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
-		Private nwindDataSet As nwindDataSet
-		Private productsBindingSource As System.Windows.Forms.BindingSource
-		Private productsTableAdapter As S131432.nwindDataSetTableAdapters.ProductsTableAdapter
-		Private colProductID As DevExpress.XtraGrid.Columns.GridColumn
-		Private colProductName As DevExpress.XtraGrid.Columns.GridColumn
-		Private colSupplierID As DevExpress.XtraGrid.Columns.GridColumn
-		Private colCategoryID As DevExpress.XtraGrid.Columns.GridColumn
-		Private colQuantityPerUnit As DevExpress.XtraGrid.Columns.GridColumn
-	End Class
+        Private gridControl1 As DevExpress.XtraGrid.GridControl
+        Private WithEvents gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+        Private productsBindingSource As System.Windows.Forms.BindingSource
+        Private colProductID As DevExpress.XtraGrid.Columns.GridColumn
+        Private colProductName As DevExpress.XtraGrid.Columns.GridColumn
+        Private colSupplierID As DevExpress.XtraGrid.Columns.GridColumn
+        Private colCategoryID As DevExpress.XtraGrid.Columns.GridColumn
+        Private colQuantityPerUnit As DevExpress.XtraGrid.Columns.GridColumn
+    End Class
 End Namespace
-
